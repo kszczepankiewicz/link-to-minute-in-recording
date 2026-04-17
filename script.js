18c07de14c35fb946b7aed668b00754657b4dd25
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-17 21:03:31 +0200

    fix(openUrl): add .replace() to replace spaces with +

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 'use strict';
-const URL = `https://www.google.com/search?&q=link+to+the+minute+in+a+recording+with+`;
+const URL = `https://www.google.com/search?q=link+do+minuty+w+nagraniu`;
+// const URL = `https://www.google.com/search?&q=link+to+the+minute+in+a+recording`;
+const DEFAULT_SEPARATOR = 'gdzie';
+// const DEFAULT_SEPARATOR = 'where';
+const SEPARATORS = ['z', 'o', 'gdzie', 'opisujacej'];
+// const SEPARATORS = ['with', 'where', 'on', 'addressing', 'about', 'describing'];
+
 
 const issueForm = document.getElementById('issue-form');
 const issueInput = document.getElementById('issue-input');
 const issueHistory = document.getElementById('issue-history');
+const issueSeparator = document.getElementById('issue-separator');
 
-const openUrl = (constantUrl, variableUrl) => window.open(constantUrl + variableUrl);
+const openUrl = (constantUrl, variableUrl, separator = DEFAULT_SEPARATOR) => window.open(`${constantUrl} ${separator} ${variableUrl}`.replace(/ /g, '+'));
 
 issueForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const { value } = issueInput;
-    issueHistory.innerHTML += value + '\n';
-    openUrl(URL, value);
+    const separatorValue = issueSeparator.value.toLowerCase() || DEFAULT_SEPARATOR;
+    issueHistory.innerHTML += separatorValue + ' ' + value + '\n';
+    if (!SEPARATORS.includes(separatorValue)) {
+        alert('Wrong separator');
+        return;
+    }
+    openUrl(URL, value, separatorValue);
 });
