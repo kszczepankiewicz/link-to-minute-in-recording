c4dfb60d709b13c628a46f75e180ee5d725c7fc5
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-17 19:00:37 +0200

    feat(openUrl): add opening url

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 'use strict';
+const URL = `https://www.google.com/search?&q=link+to+the+minute+in+a+recording+with+`;
+
 const issueForm = document.getElementById('issue-form');
 const issueInput = document.getElementById('issue-input');
 const issueHistory = document.getElementById('issue-history');
 
+const openUrl = (constantUrl, variableUrl) => window.open(constantUrl + variableUrl);
+
 issueForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    issueHistory.innerHTML += issueInput.value + '\n';
+    const { value } = issueInput;
+    issueHistory.innerHTML += value + '\n';
+    openUrl(URL, value);
 });
